e9b49fac55b5cfa4f343d46030417592c1a909fd
michal <michal.d.gornicki@gmail.com>
2023-01-06 20:20:00 +0000

    update App file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';  
-import Navigation from './components/Navigation';
-import Login from './components/Login';
-import MainMenu from './components/MainMenu';
-import Communication from './components/Communication';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./myCSS.css";
+import Navigation from "./components/Navigation";
+import Login from "./pages/Login";
+import MainMenu from "./components/MainMenu";
+import Communication from "./components/Communication";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import Welcome from "./components/Welcome";
+import Test from "./components/Test";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Connect from "./pages/Connect";
 
 function App() {
   return (
-    <div className='App'>
-      <Navigation/>
-      <MainMenu/>
-      <Communication/>
-      <Login/>
+    <div className="App">
+      <Connect />
     </div>
   );
 }
