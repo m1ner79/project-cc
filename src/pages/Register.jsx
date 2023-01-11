29660536facecb6bcbf7a2d442f63abbdb53a764
michal <michal.d.gornicki@gmail.com>
2023-01-11 01:13:22 +0000

    update Registare page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,67 @@
 import React, { useState } from "react";
 import { Form, Button, Container, Card, Image } from "react-bootstrap";
 import validator from "validator";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
-function Register() {
+
+
+const Register = () => {
+  const [signupError, setSinupError] = useState(false);
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
+
+    //event.preventDefault();
+    const firstName = event.target[0].value;
+    const lastName = event.target[1].value;
+    const email = event.target[2].value;
+    const password = event.target[3].value;
+    const file = event.target[4].files[0];
+    const fullName = firstName + " " + lastName;
+
     const form = event.currentTarget;
+
+    try {
+       //Create a user
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, fullName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+
+      uploadTask.on(
+        
+        (error) => {
+          
+          // Handle unsuccessful uploads
+          setSinupError(true);
+        },
+        () => {
+          // Handle successful uploads on complete
+          getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+            await updateProfile(response.user, {
+              firstName,
+              lastName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", response.user.uid), {
+              uid: response.user.uid,
+              firstName,
+              lastName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (signupError) {
+      setSinupError(true);
+    }
+
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
@@ -63,17 +118,16 @@ function Register() {
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
-              <Form.Control 
-              required 
-              type="email" 
-              placeholder="Email"
-              onChange={(e) => validateEmail(e)}
+              <Form.Control
+                required
+                type="email"
+                placeholder="Email"
+                onChange={(e) => validateEmail(e)}
               />
               <Form.Control.Feedback>{emailError}</Form.Control.Feedback>
               <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
               </Form.Text>
-
             </Form.Group>
             <Form.Group className="mb-3" controlId="validationCustom03">
               <Form.Label>Password</Form.Label>
@@ -94,14 +148,19 @@ function Register() {
               <Form.Control type="file" required name="file" />
               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
             </Form.Group>
-
             <Button variant="primary" type="submit">
               Sign Up
             </Button>
+            {signupError && (
+              <span>Some details were incorrect. Try again.</span>
+            )}
           </Form>
         </Card.Body>
         <Card.Footer className="text-muted">
-          Are you already registered? <b>Login</b>
+          Are you already registered?{" "}
+          <a href="/Login">
+            <b>Login</b>
+          </a>
         </Card.Footer>
       </Card>
     </Container>
