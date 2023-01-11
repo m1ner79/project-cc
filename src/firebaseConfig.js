9354f58cd44fe38dff35742a81f68e9e7732130d
michal <michal.d.gornicki@gmail.com>
2023-01-11 01:12:02 +0000

    update firebase config file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,11 +4,8 @@ import {getAuth} from "firebase/auth";
 import {getStorage} from "firebase/storage";
 import {getFirestore} from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAqqEIcXcSTbE8F1b52OqNn4r-NlYmKsHg",
   authDomain: "ccdb-f69e9.firebaseapp.com",
