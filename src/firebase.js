2dc5d7292e424f31b146c56be4351ed077cffb0a
michal <michal.d.gornicki@gmail.com>
2023-01-16 21:55:40 +0000

    update exports

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth";
+import {getStorage} from "firebase/storage";
+import {getFirestore} from "firebase/firestore";
 
 // web app's Firebase configuration
 const firebaseConfig = {
