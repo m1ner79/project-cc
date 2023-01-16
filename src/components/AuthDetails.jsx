e751e4246c6a07b18e990e9163fe1edbdeecadbb
michal <michal.d.gornicki@gmail.com>
2023-01-16 17:54:52 +0000

    add auth component

---
+++ src/components/AuthDetails.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState, createContext } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
+export const AuthDetails = createContext();
+
+export const AuthInfo = ({ children }) => {
+  const [loggedUser, setLoggedUser] = useState(null);
+
+  useEffect(() => {
+    //listening in real time
+    const checkStatus = onAuthStateChanged(auth, (user) => {
+        setLoggedUser(user);
+        console.log(user);
+    });
+    // clean up function to prevent memory leaking
+    return () => {
+      checkStatus();
+    };
+  }, []);
+
+  return (
+    <AuthInfo value={{ loggedUser }}>
+      {children}
+    </AuthInfo>
+  );
+};
