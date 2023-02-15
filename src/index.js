0e6210e694b1a4ca77f7dcea3dafb7f2d0d7c25b
michal <michal.d.gornicki@gmail.com>
2023-02-15 17:20:33 +0000

    update index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { AuthInfo } from './components/AuthDetails';
-import {MessageInfo} from "./components/MessageDetails";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthInfo } from "./components/AuthDetails";
+import { MessageInfo } from "./components/MessageDetails";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthInfo>
-      <MessageInfo>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>
-      </MessageInfo>
+    <MessageInfo>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MessageInfo>
   </AuthInfo>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
