ae998a63a94540fc9d0d5fcfebfca25ab4e6ea4a
michal <michal.d.gornicki@gmail.com>
2023-04-05 16:51:51 +0100

    updated routing to Update Child component

--- src/App.js
+++ src/App.js
@@ -6,61 +6,75 @@ import "./customStyle.scss";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import { AuthDetails} from "./components/AuthDetails";
+import { AuthDetails } from "./components/AuthDetails";
 import Connect from "./pages/Connect";
 import AddChild from "./components/AddChild";
+import UpdateChild from "./components/UpdateChild";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  const { loggedUser } = useContext(AuthDetails);
 
-  const {loggedUser} = useContext(AuthDetails);
+  const AuthRoute = ({ children, allowedRoles }) => {
+    if (loggedUser && allowedRoles.includes(loggedUser.userRole)) {
+      return children;
+    } else if (loggedUser) {
+      return <Navigate to="/connect" />;
+    } else {
+      return <Navigate to="/login" />;
+    }
+  };
 
-    const AuthRoute = ({ children, allowedRoles }) => {
-        if (loggedUser && allowedRoles.includes(loggedUser.userRole)) {
-            return children;
-        } else if (loggedUser) {
-            return <Navigate to="/connect" />;
-        } else {
-            return <Navigate to="/login" />;
-        }
-    };
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/">
-                    <Route
-                        index
-                        element={
-                            <AuthRoute allowedRoles={["manager", "staff"]}>
-                                <Home />
-                            </AuthRoute>
-                        }
-                    />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                </Route>
+  const updateChild = async (updatedChild) => {
+    try {
+      const childRef = doc(db, "children", updatedChild.id);
+      await updateDoc(childRef, updatedChild);
+    } catch (error) {
+      console.error("Error updating child: ", error);
+    }
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <AuthRoute allowedRoles={["manager", "staff"]}>
+                <Home />
+              </AuthRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
 
-                <Route path="/connect">
-                    <Route
-                        index
-                        element={
-                            <AuthRoute allowedRoles={["parent", "manager", "staff"]}>
-                                <Connect />
-                            </AuthRoute>
-                        }
-                    />
-                </Route>
-                <Route path="/addchild">
-                    <Route
-                        index
-                        element={
-                            <AuthRoute allowedRoles={["manager", "staff"]}>
-                                <AddChild />
-                            </AuthRoute>
-                        }
-                    />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <Route path="/connect">
+          <Route
+            index
+            element={
+              <AuthRoute allowedRoles={["parent", "manager", "staff"]}>
+                <Connect />
+              </AuthRoute>
+            }
+          />
+        </Route>
+        <Route path="/addchild">
+          <Route
+            index
+            element={
+              <AuthRoute allowedRoles={["manager", "staff"]}>
+                <AddChild />
+              </AuthRoute>
+            }
+          />
+        </Route>
+        <Route path="/update/:id">
+          <Route index element={<UpdateChild onUpdate={updateChild} />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
