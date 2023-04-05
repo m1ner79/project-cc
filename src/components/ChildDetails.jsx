9c0aea8f76248c239cfe1ef9420bad356d083e68
michal <michal.d.gornicki@gmail.com>
2023-04-05 12:10:15 +0100

    add buttons to handle delete/update

--- src/components/ChildDetails.jsx
+++ src/components/ChildDetails.jsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
-const ChildDetails = ({ child }) => {
+const ChildDetails = ({ child, updateChild, removeChild }) => {
+  let fullName = `${child.lowFirstName} ${child.lowLastName}`; // combine first and last name
+  let fullNameCaps = fullName.toUpperCase(); // convert to uppercase
   return (
     <Card>
       <Card.Header className="formCard" as="h5">
-        {child.lowFirstName} {child.lowLastName}
+        {fullNameCaps}
       </Card.Header>
       <Card.Body>
-      <Card.Text>Date of Birth: {child.dob}</Card.Text>
-      <Card.Text>Parent Name: {child.parentName}</Card.Text>
-      <Card.Text>Parent Email: {child.parentEmail}</Card.Text>
-      <Card.Text>Parent Mobile Number: {child.parentMobile}</Card.Text>
-      <Card.Text>Child's Health Info: {child.healthInfo}</Card.Text>
+      <Card.Text><b>Date of Birth:</b> <br></br>{child.dob}</Card.Text>
+      <Card.Text><b>Parent Name:</b> <br></br>{child.parentName}</Card.Text>
+      <Card.Text><b>Parent Email:</b> <br></br>{child.parentEmail}</Card.Text>
+      <Card.Text><b>Parent Mobile Number:</b> <br></br>{child.parentMobile}</Card.Text>
+      <Card.Text><b>Child's Health Info:</b> <br></br>{child.healthInfo}</Card.Text>
+      <Card.Text><b>Observations:</b> <br></br>{child.observations}</Card.Text>
+      <Card.Text></Card.Text>
       </Card.Body>
+      <Card.Footer>
+      <Button variant="primary" onClick={() => updateChild(child.id)}>Update</Button>
+      <Button variant="danger" onClick={() => removeChild(child.id)}>Remove</Button>
+      </Card.Footer>
     </Card>
   );
 };
