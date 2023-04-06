e887119fa94a921c0e0e428d6d4115363758ff68
michal <michal.d.gornicki@gmail.com>
2023-04-07 00:22:52 +0100

    add extra button

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -7,14 +7,20 @@ function MainMenu() {
       <Row className="text-center">
         <Col md={{ span: 6, offset: 3 }}>
         <Link to="/addchild">
-        <Button className="addChildButton" variant="primary" size="lg" style={{marginRight: 5, marginTop: 10}}>
+        <Button className="addChildButton" variant="primary" size="lg" style={{margin: 5}}>
           Add a child
         </Button>
         </Link>
 
         <Link to="/connect">
-          <Button className="connectButton" variant="primary" size="lg" style={{marginTop: 10}}>
+          <Button className="connectButton" variant="primary" size="lg" style={{margin: 5}}>
             Message
+          </Button>
+        </Link>
+
+        <Link to="/archive">
+          <Button className="archiveButton" variant="primary" size="lg" style={{margin: 5}}>
+            Archive
           </Button>
         </Link>
         </Col>
