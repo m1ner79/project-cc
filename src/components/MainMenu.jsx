a6295e711184041828598b4f3f6f92eedab02a6b
michal <michal.d.gornicki@gmail.com>
2023-04-07 22:48:40 +0100

    updated addChild button for manager only

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,16 +1,21 @@
+import React, {useContext} from "react";
 import { Container, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {AuthDetails} from "./AuthDetails";
 
 function MainMenu() {
+  const { loggedUser } = useContext(AuthDetails);
   return (
     <Container className="mainmenu">
       <Row className="text-center">
         <Col md={{ span: 6, offset: 3 }}>
+        {loggedUser && (loggedUser.userRole === "manager" )&& (
         <Link to="/addchild">
         <Button className="addChildButton" variant="primary" size="lg" style={{margin: 5}}>
           Add a child
         </Button>
         </Link>
+        )}
 
         <Link to="/connect">
           <Button className="connectButton" variant="primary" size="lg" style={{margin: 5}}>
