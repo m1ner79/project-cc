de659a7c607fcbdc2f1d51f36c4f4ce40dc1af06
michal <michal.d.gornicki@gmail.com>
2023-04-14 17:59:41 +0100

    update connect page

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,49 +1,43 @@
-import React , {useContext} from "react";
-import {Container, Button, Col, Row, Card, CardGroup} from "react-bootstrap";
+import React, {useContext} from "react";
+import {Container, Button, Card, CardGroup} from "react-bootstrap";
 import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
 import Navigation from "../components/Navigation";
 import {Link} from "react-router-dom";
-import { AuthDetails } from "../components/AuthDetails";
+import {AuthDetails} from "../components/AuthDetails";
 
 
 function Connect() {
-  const { loggedUser } = useContext(AuthDetails);
+    const {loggedUser} = useContext(AuthDetails);
 
-  console.log("user",loggedUser);
+    console.log("user", loggedUser);
 
-  return (
-    <>
-    <Navigation />
-      <Container className="connect">
-        <Container className="wrapper">
-        <CardGroup className="connectArea">
-      <Card className="sidebarCard">
-        <Card.Body>
-          <Card.Text>
-          <Sidebar />
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="chatCard">
-        <Card.Body>
-          <Card.Text>
-          <Chat />
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </CardGroup>
-          {loggedUser && (loggedUser.userRole === "staff" || loggedUser.userRole === "manager") && (
-            <Link to="/">
-              <Button className="connectButton" variant="primary" size="lg">
-                Back to Main Menu
-              </Button>
-            </Link>
-          )}
-        </Container>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Container className="connect">
+                <Container className="wrapper">
+                    <CardGroup className="connectArea">
+                        <Card className="sidebarCard">
+                            <Sidebar/>
+                        </Card>
+                        <Card className="chatCard">
+                            <Chat/>
+                        </Card>
+                    </CardGroup>
+                    {loggedUser && (loggedUser.userRole === "staff" || loggedUser.userRole === "manager") && (
+                        <Container className="text-center">
+                        <Link to="/">
+                            <Button className="connectButton" variant="primary" size="lg" style={{marginTop: 10}}>
+                                Back to Main Menu
+                            </Button>
+                        </Link>
+                    </Container>
+                    )}
+                </Container>
+            </Container>
+        </>
+    );
 }
 
 export default Connect;
