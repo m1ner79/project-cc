09e3776d781b2d2641231404cbea936011ac0ee9
michal <michal.d.gornicki@gmail.com>
2023-04-15 12:58:56 +0100

    update button style

--- src/components/UpdateChild.jsx
+++ src/components/UpdateChild.jsx
@@ -160,7 +160,7 @@ const UpdateChild = ({ onUpdate }) => {
         <Container className="text-center">
         <Link to="/">
             <Button 
-            className="connectButton" variant="primary" size="lg" style={{marginTop: 10}}>
+            className="connectButton" variant="primary" size="lg" style={{margin: 5}}>
               Back to Main Menu
             </Button>
           </Link>
